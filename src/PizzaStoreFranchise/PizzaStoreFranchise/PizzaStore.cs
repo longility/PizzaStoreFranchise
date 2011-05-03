263b3602b7a85b336de484a0c41e945a5c7741e1
Long Mai <long.mai@longility.com>
2011-05-02 21:34:37 -0500

    Created new project with creation of only one pizza

---
+++ src/PizzaStoreFranchise/PizzaStoreFranchise/PizzaStore.cs
@@ -0,0 +1,18 @@
+﻿
+namespace PizzaStoreFranchise
+{
+    class PizzaStore
+    {
+        public Pizza OrderPizza()
+        {
+            Pizza pizza = new Pizza();
+
+            pizza.Prepare();
+            pizza.Bake();
+            pizza.Cut();
+            Pizza.Box();
+
+            return pizza;
+        }
+    }
+}
